89be372514b403bc867dbf66a691ba3767c28d2a
ANURAG VEMULA <101939604+Smokeengine@users.noreply.github.com>
2026-03-19 19:39:03 -0600

    Use environment variable for API URL

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,6 +1,5 @@
 import axios from "axios";
-const API_URL = "http://localhost:8800/api-v1";
-
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8800/api-v1";
 export const API = axios.create({
   baseURL: API_URL,
   responseType: "json",
