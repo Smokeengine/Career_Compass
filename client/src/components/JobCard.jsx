522dd50a2de149fc466e0047d929b10dedfe0ed0
ANURAG VEMULA <101939604+Smokeengine@users.noreply.github.com>
2026-03-21 06:49:21 -0600

    Update JobCard.jsx

--- client/src/components/JobCard.jsx
+++ client/src/components/JobCard.jsx
@@ -23,14 +23,14 @@ const JobCard = ({ job }) => {
       <div className='w-full h-full flex flex-col justify-between'>
         <div className='flex gap-3'>
           <img
-  src={logo}
-  alt={job?.name || job?.company?.name}
-  className='w-14 h-14 object-contain rounded'
-  loading="lazy"
-  onError={(e) => { e.target.src = noLogo }}
-/>
-          <div className='w-full h-16 flex flex-col justify-center'>
-            <p className='w-full h-12 flex items-center text-lg font-semibold overflow-hidden leading-5'>
+              src={logo}
+              alt={job?.name || job?.company?.name}
+              className='w-14 h-14 object-contain rounded'
+              loading="lazy"
+              onError={(e) => { e.target.src = noLogo }}
+            />
+          <div className='w-full h-20 flex flex-col justify-center'>
+            <p className='w-full h-16 flex items-center text-lg font-semibold overflow-hidden leading-5'>
               {job?.jobTitle}
             </p>
             <span className='flex gap-2 items-center'>
